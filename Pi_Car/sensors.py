5835441a820b86da3e225b4915b150b2a76d8bc3
abugowski <abugowski@gmail.com>
2022-05-18 21:29:12 +0200

    Sensor boot

--- Pi_Car/sensors.py
+++ Pi_Car/sensors.py
@@ -1,5 +1,6 @@
 from flask import current_app as app
 import board
+from gpiozero import Button, exc
 
 try:
     from w1thermsensor import W1ThermSensor
@@ -37,7 +38,7 @@ class Sensors:
         return int(temperature)
 
     # DHT22 -> sensor
-    # @staticmethod
+    @staticmethod
     def get_dht_temp():
         """
         Safely read the external temperature from DHT22 sensor
@@ -62,3 +63,34 @@ class Sensors:
         app.logger.info("Finished reading temperature sensor")
         app.logger.debug(f"Temperature: {temperature_dht}")
         return int(temperature_dht)
+
+    @staticmethod
+    def get_boot_status():
+        """
+        Safely read the boot sensor and calculate the state - open/closed/unknow
+        :return: String - boot status
+        """
+        app.logger.info("Starting to read boot sensor")
+        result = None
+        status = None
+
+        try:
+            button = Button(pin=14)
+            status = button.is_pressed
+        except exc.BadPinFactory as e:
+            app.logger.warning(f"Unable to use boot sensor in this environment: {e}")
+            result = "Unknown"
+        except Exception as e:
+            app.logger.error(f"Unknown problem with boot sensor: {e}")
+            result = "Unknown"
+
+        if not result:
+            if status:
+                result = "Closed"
+            else:
+                result = "Open"
+
+        app.logger.debug(f"Boot: {result}")
+        app.logger.info("Finished reading boot sensor")
+        return result
+
